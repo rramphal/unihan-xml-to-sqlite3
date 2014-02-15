bf499b6bc60ce1b687719e0ccd9071ca12af50e7
Ravi S. Rāmphal <rramphal@gmail.com>
2014-02-14 19:48:47 -0800

    Set up class to hold character objects to be stored in array

--- unihan_xml_to_sqlite3.rb
+++ unihan_xml_to_sqlite3.rb
@@ -1,14 +1,22 @@
-unihan = File.open("ucd.unihan.flat copy.xml").read
-
 class Character
-  def initialize()
-    @kCantonese = String.new
-    @kDefinition = String.new
-    @kMandarin = String.new
-    @kCangjie = String.new
-    @kRSUnicode = String.new
+  def initialize(kCantonese, kDefinition, kMandarin, kHanyuPinyin, kCangjie, kRSKangXi)
+    @kCantonese = kCantonese # Jyutping
+    @kDefinition = kDefinition # Gloss
+    @kMandarin = kMandarin # Most common pinyin
+    @kHanyuPinyin = split_pinyin(kHanyuPinyin) # Pinyin
+    @kCangjie = kCangjie # Cangjie
+    @kRSKangXi = split_radical(kRSKangXi) # Radical
   end
 
   def split_pinyin(pinyin)
-    
+
   end
+
+  def split_radical(kangxi)
+    kangxi.matches(/(\D)+\..*/)
+  end
+end
+
+unihan = Array.new
+
+unihan_file = File.open("ucd.unihan.flat copy.xml").read
