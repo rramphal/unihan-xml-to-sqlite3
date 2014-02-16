7b4f50d7db02b0f1da0525a4734886af5bfcc2c5
Ravi S. Rāmphal <rramphal@gmail.com>
2014-02-16 12:58:53 -0800

    switched to real xml and created database

--- unihan_xml_to_sqlite3.rb
+++ unihan_xml_to_sqlite3.rb
@@ -1,22 +1,44 @@
 require 'sqlite3'
 
-@db = SQLite3::Database.new( "unihan.db" )
+@db = SQLite3::Database.new( "unihan.sqlite" )
 
-characters = Hash.new
+@db.execute( "DROP TABLE IF EXISTS characters" )
 
-unihan_file = File.readlines("ucd.unihan.flat copy.xml")
+@db.execute( "CREATE TABLE characters
+                ( character VARCHAR,
+                  simplified VARCHAR,
+                  pinyin VARCHAR,
+                  kDefinition TEXT,
+                  readings TEXT,
+                  jyutping VARCHAR,
+                  radical INT,
+                  cangjie VARCHAR,
+                  codepoint VARCHAR )" )
+
+unihan_file = File.readlines("ucd.unihan.flat.xml")
 
 unihan_file.each do |line|
+  # RAW UNICODE RESULTS
   kSimplifiedVariant = line[/ kSimplifiedVariant="(.*?)"/] ? / kSimplifiedVariant="(.*?)"/.match(line)[1] : nil # simplfied
   kMandarin          = line[/ kMandarin="(.*?)"/]          ? / kMandarin="(.*?)"/.match(line)[1]          : nil # most common pinyin
   kDefinition        = line[/ kDefinition="(.*?)"/]        ? / kDefinition="(.*?)"/.match(line)[1]        : nil # gloss
   kHanyuPinyin       = line[/ kHanyuPinyin="(.*?)"/]       ? / kHanyuPinyin="(.*?)"/.match(line)[1]       : nil # pinyin
   kCantonese         = line[/ kCantonese="(.*?)"/]         ? / kCantonese="(.*?)"/.match(line)[1]         : nil # jyutping
-  kRSUnicode         = line[/ kRSUnicode="(.*?)"/]         ? / kRSUnicode="(.*?)"/.match(line)[1].to_i    : nil # radical
+  kRSUnicode         = line[/ kRSUnicode="(.*?)"/]         ? / kRSUnicode="(.*?)"/.match(line)[1]         : nil # radical
   kCangjie           = line[/ kCangjie="(.*?)"/]           ? / kCangjie="(.*?)"/.match(line)[1]           : nil # cangjie
   cp                 = line[/ cp="(.*?)"/]                 ? / cp="(.*?)"/.match(line)[1]                 : nil # codepoint
 
+  # TRANSFORMATIONS
+  kSimplifiedVariant = kSimplifiedVariant.nil? ? nil       : (/^U+(.*)$/.match(kSimplifiedVariant)[1]).to_i(16).chr('UTF-8')
+  kRSUnicode         = kRSUnicode.nil?         ? nil       : kRSUnicode.to_i
+  kHanyuPinyin       = kHanyuPinyin.nil?       ? kMandarin : /^.*?:(.*)$/.match(kHanyuPinyin)[1]
+
+  # INSERT INTO DATABASE
   if not cp.nil?
-    character = cp.to_i(16).chr('UTF-8')
+    character = cp.to_i(16).chr('UTF-8') # generates character from codepoint
+     @db.execute( "INSERT INTO characters (character, simplified, pinyin, kDefinition, readings, jyutping, radical, cangjie, codepoint)
+                    VALUES ( ? , ? , ? , ? , ? , ? , ? , ? , ? )",
+                      character, kSimplifiedVariant, kMandarin, kDefinition, kHanyuPinyin, kCantonese, kRSUnicode, kCangjie, cp )
+    print character
   end
 end
