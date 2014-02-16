5b7fc5756a3c52e0a4ecad5e4c0bf7b28d8dcf90
Ravi S. Rāmphal <rramphal@gmail.com>
2014-02-16 10:41:55 -0800

    removed intermediary object step, set up database, ordered based off anki order, formatted

--- unihan_xml_to_sqlite3.rb
+++ unihan_xml_to_sqlite3.rb
@@ -1,22 +1,30 @@
-class Character
-  def initialize(kCantonese, kDefinition, kMandarin, kHanyuPinyin, kCangjie, kRSKangXi)
-    @kCantonese = kCantonese # Jyutping
-    @kDefinition = kDefinition # Gloss
-    @kMandarin = kMandarin # Most common pinyin
-    @kHanyuPinyin = split_pinyin(kHanyuPinyin) # Pinyin
-    @kCangjie = kCangjie # Cangjie
-    @kRSKangXi = split_radical(kRSKangXi) # Radical
-  end
+require 'sqlite3'
 
-  def split_pinyin(pinyin)
+@db = SQLite3::Database.new( "unihan.db" )
 
-  end
+characters = Hash.new
 
-  def split_radical(kangxi)
-    kangxi.matches(/(\D)+\..*/)
+unihan_file = File.readlines("ucd.unihan.flat copy.xml")
+
+unihan_file.each do |line|
+  kSimplifiedVariant = line[/ kSimplifiedVariant="(.*?)"/] ? / kSimplifiedVariant="(.*?)"/.match(line)[1] : nil # simplfied
+  kMandarin          = line[/ kMandarin="(.*?)"/]          ? / kMandarin="(.*?)"/.match(line)[1]          : nil # most common pinyin
+  kDefinition        = line[/ kDefinition="(.*?)"/]        ? / kDefinition="(.*?)"/.match(line)[1]        : nil # gloss
+  kHanyuPinyin       = line[/ kHanyuPinyin="(.*?)"/]       ? / kHanyuPinyin="(.*?)"/.match(line)[1]       : nil # pinyin
+  kCantonese         = line[/ kCantonese="(.*?)"/]         ? / kCantonese="(.*?)"/.match(line)[1]         : nil # jyutping
+  kRSUnicode         = line[/ kRSUnicode="(.*?)"/]         ? / kRSUnicode="(.*?)"/.match(line)[1].to_i    : nil # radical
+  kCangjie           = line[/ kCangjie="(.*?)"/]           ? / kCangjie="(.*?)"/.match(line)[1]           : nil # cangjie
+  cp                 = line[/ cp="(.*?)"/]                 ? / cp="(.*?)"/.match(line)[1]                 : nil # codepoint
+
+  if not cp.nil?
+    character = cp.to_i(16).chr('UTF-8')
+    characters[character] = Character.new( kSimplifiedVariant,
+                                           kMandarin,
+                                           kDefinition,
+                                           kHanyuPinyin,
+                                           kCantonese,
+                                           kRSUnicode,
+                                           kCangjie,
+                                           cp )
   end
 end
-
-unihan = Array.new
-
-unihan_file = File.open("ucd.unihan.flat copy.xml").read
