d4af20e7d7fdc1f10c4d965d139f5b673fa4973a
Ravi S. Rāmphal <rramphal@gmail.com>
2014-02-18 11:51:39 -0800

    more efficient search for fields, consolidiated kMandarin and kHanyuPinyin

--- unihan_xml_to_sqlite3.rb
+++ unihan_xml_to_sqlite3.rb
@@ -7,38 +7,64 @@ require 'sqlite3'
 @db.execute( "CREATE TABLE characters
                 ( character VARCHAR,
                   simplified VARCHAR,
-                  pinyin VARCHAR,
                   kDefinition TEXT,
                   readings TEXT,
                   jyutping VARCHAR,
                   radical INT,
                   cangjie VARCHAR,
-                  codepoint VARCHAR )" )
+                  code VARCHAR )" )
 
 unihan_file = File.readlines("ucd.unihan.flat.xml")
 
 unihan_file.each do |line|
   # RAW UNICODE RESULTS
-  kSimplifiedVariant = line[/ kSimplifiedVariant="(.*?)"/] ? / kSimplifiedVariant="(.*?)"/.match(line)[1] : nil # simplfied
-  kMandarin          = line[/ kMandarin="(.*?)"/]          ? / kMandarin="(.*?)"/.match(line)[1]          : nil # most common pinyin
-  kDefinition        = line[/ kDefinition="(.*?)"/]        ? / kDefinition="(.*?)"/.match(line)[1]        : nil # gloss
-  kHanyuPinyin       = line[/ kHanyuPinyin="(.*?)"/]       ? / kHanyuPinyin="(.*?)"/.match(line)[1]       : nil # pinyin
-  kCantonese         = line[/ kCantonese="(.*?)"/]         ? / kCantonese="(.*?)"/.match(line)[1]         : nil # jyutping
-  kRSUnicode         = line[/ kRSUnicode="(.*?)"/]         ? / kRSUnicode="(.*?)"/.match(line)[1]         : nil # radical
-  kCangjie           = line[/ kCangjie="(.*?)"/]           ? / kCangjie="(.*?)"/.match(line)[1]           : nil # cangjie
-  cp                 = line[/ cp="(.*?)"/]                 ? / cp="(.*?)"/.match(line)[1]                 : nil # codepoint
+  kSimplifiedVariant = line.include?("kSimplifiedVariant") ? / kSimplifiedVariant="(.*?)"/.match(line)[1] : nil # simplfied
+  kMandarin          = line.include?("kMandarin")          ? / kMandarin="(.*?)"/.match(line)[1]          : nil # most common pinyin
+  kDefinition        = line.include?("kDefinition")        ? / kDefinition="(.*?)"/.match(line)[1]        : nil # gloss
+  kHanyuPinyin       = line.include?("kHanyuPinyin")       ? / kHanyuPinyin="(.*?)"/.match(line)[1]       : nil # pinyin
+  kCantonese         = line.include?("kCantonese")         ? / kCantonese="(.*?)"/.match(line)[1]         : nil # jyutping
+  kRSUnicode         = line.include?("kRSUnicode")         ? / kRSUnicode="(.*?)"/.match(line)[1]         : nil # radical
+  kCangjie           = line.include?("kCangjie")           ? / kCangjie="(.*?)"/.match(line)[1]           : nil # cangjie
+  cp                 = line.include?("cp")                 ? / cp="(.*?)"/.match(line)[1]                 : nil # codepoint
 
   # TRANSFORMATIONS
-  kSimplifiedVariant = kSimplifiedVariant.nil? ? nil       : (/^U+(.*)$/.match(kSimplifiedVariant)[1]).to_i(16).chr('UTF-8')
-  kRSUnicode         = kRSUnicode.nil?         ? nil       : kRSUnicode.to_i
-  kHanyuPinyin       = kHanyuPinyin.nil?       ? kMandarin : /^.*?:(.*)$/.match(kHanyuPinyin)[1]
+  kSimplifiedVariant = kSimplifiedVariant.nil? ? nil : (/^U+(.*)$/.match(kSimplifiedVariant)[1]).to_i(16).chr('UTF-8')
+    # this parses out the codepoint of the simplified version and converts it from hex to decimal and then to the character
+  kRSUnicode         = kRSUnicode.nil?         ? nil : kRSUnicode.to_i
+    # this converts the radical.stroke string to an integer, cutting off the decimal (the strokes)
 
+  kHanyuPinyin = kMandarin if kHanyuPinyin.nil?
+    # if kHanyuPinyin is empty, copy kMandarin into it (even if kMandarin is nil itself)
+
+  if not kHanyuPinyin.nil? # as long as there is something to work with in kHanyuPinyin
+    # this accounts for readings listed as "10585.010:kuáng 10589.110:chéng,chěng"
+    kHanyuPinyin.gsub!(/ /, ',')
+    # replace spaces with commas
+    kHanyuPinyin.gsub!(/(?<=\d),(?=\d)/, '')
+    # remove commas between digits as in readings like "20961.010,20961.020:yì"
+    kHanyuPinyin.gsub!(/[\d\.:]/, '')
+    # remove digits, periods, and colons
+    kHanyuPinyin.gsub(/ /, '')
+    # remove any spaces (just in case)
+    kHanyuPinyin = kHanyuPinyin.split(',')
+    # split the string into an array based on the commas
+    kHanyuPinyin.unshift(kMandarin) unless kMandarin.include?(',')
+    # push kMandarin to the beginning of kHanyuPinyin (unless is just in case)
+    kHanyuPinyin.compact!
+    # remove any nils (just in case)
+    kHanyuPinyin.uniq!
+    # remove any duplicates (will be many since we added kMandarin to kHanyuPinyin)
+    kHanyuPinyin = kHanyuPinyin.join(",")
+  end
+  
   # INSERT INTO DATABASE
   if not cp.nil?
     character = cp.to_i(16).chr('UTF-8') # generates character from codepoint
-     @db.execute( "INSERT INTO characters (character, simplified, pinyin, kDefinition, readings, jyutping, radical, cangjie, codepoint)
-                    VALUES ( ? , ? , ? , ? , ? , ? , ? , ? , ? )",
-                      character, kSimplifiedVariant, kMandarin, kDefinition, kHanyuPinyin, kCantonese, kRSUnicode, kCangjie, cp )
+     @db.execute( "INSERT INTO characters (character, simplified, kDefinition, readings, jyutping, radical, cangjie, code)
+                    VALUES ( ? , ? , ? , ? , ? , ? , ? , ? )",
+                      character, kSimplifiedVariant, kDefinition, kHanyuPinyin, kCantonese, kRSUnicode, kCangjie, cp )
     print character
   end
 end
+
+print "\n"
